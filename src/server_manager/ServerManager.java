cb1a263abae1b8de71a850f34a5bc70920966a41
Andrew Monteith <andrew.monteith1@gmail.com>
2015-06-19 03:31:30 +1000

    Refactored to be more readable

--- src/server_manager/ServerManager.java
+++ src/server_manager/ServerManager.java
@@ -7,95 +7,80 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class ServerManager {
-
-    static int[] predictedUsers = new int[31]; // predicted hits per day
-    static int[] bandwidthPerDay = new int[31]; // bandwidth required per day
-    static int[] serversPerDay = new int[31]; // peak number of servers required each day
-    static double[] costPerDayOnDemand = new double[31]; // On-Demand only pricing
-    static double[] costPerDayCombined = new double[31]; // On-Demand/Reserved pricing
-    static double onDemandHourlyCost = 0.532; // $ Per hour per server on demand
-    static double reservedHourlyCost = 0.380; // $ Per hour per server reserved
-    static int maxBandwidth = 1000; // m3.2xlarge server = 1000 Mbps throughput
-    static int videoQuality = 20; // 1080p ~ 20Mbps
-    static int avgServers;
-    static double lowestCost;
+    /*
+    * There are a lot of parameters and they were making the file hard to read
+    * so they were moved to a separate file
+    */
+    ServerManagerParameters params = new ServerManagerParameters();
 
     /**
      * Tries to find the cheapest way to host a video, based on maximum daily
      * views contained in the input file.
      *
-     * @param fileName
-     * @throws FileNotFoundException
+     * @param fileName is the file with data
      * @throws IOException
      */
-    public static void findValue(File fileName) throws FileNotFoundException,
+    public void findValue(File fileName) throws FileNotFoundException,
             IOException {
+
         int averageViews;
         int maxViews;
         readInput(fileName);
-
         averageViews = calculateAverageViews();
-        System.err.println("Average Views: " + averageViews);
 
         maxViews = getMax();
         System.err.println("Max Views: " + maxViews + "\n");
-
         calculateDailyBandwidth();
-
-        avgServers = (averageViews * videoQuality) / maxBandwidth;
-
-        calculateServersPerDay();
+        params.avgServers = (averageViews * params.videoQuality) / params.maxBandwidth;
+        calculateServersEachDay();
 
     }
 
-    static void readInput(File file) throws FileNotFoundException, IOException {
+    private void readInput(File file) throws IOException {
         Scanner input = new Scanner(new FileReader(file));
 
         for (int i = 0; i < 31; i++) {
             if (input.hasNextLine()) {
-                predictedUsers[i] = input.nextInt();
+                params.predictedUsers[i] = input.nextInt();
             }
         }
 
         input.close();
     }
-
-    static int calculateAverageViews() {
+    private int calculateAverageViews() {
         int avg, views = 0, num = 0;
 
         for (int i = 0; i < 31; i++) {
-            if (predictedUsers[i] != 0) {
+            if (params.predictedUsers[i] != 0) {
                 num++;
-                views += predictedUsers[i];
+                views += params.predictedUsers[i];
             }
         }
 
         avg = (views / num);
-
+        System.err.println("Average Views: " + avg);
         return avg;
     }
-
-    static int getMax() {
+    private int getMax() {
         int max = 0;
 
         for (int i = 0; i < 31; i++) {
-            if (predictedUsers[i] > max) {
-                max = predictedUsers[i];
+            if (params.predictedUsers[i] > max) {
+                max = params.predictedUsers[i];
             }
         }
 
         return max;
     }
-
-    static void calculateDailyBandwidth() {
+    private void calculateDailyBandwidth() {
         for (int i = 0; i < 31; i++) {
-            bandwidthPerDay[i] = (predictedUsers[i] * videoQuality);
+            params.bandwidthPerDay[i] = (params.predictedUsers[i] * params.videoQuality);
+        }
+    }
+    private void calculateServersEachDay() {
+        for (int i = 0; i < 31; i++) {
+            params.serversPerDay[i] = (params.bandwidthPerDay[i] / params.maxBandwidth);
         }
     }
 
-    static void calculateServersPerDay() {
-        for (int i = 0; i < 31; i++) {
-            serversPerDay[i] = (bandwidthPerDay[i] / maxBandwidth);
-        }
-    }
 }
